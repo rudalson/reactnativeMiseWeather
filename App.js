a2421a30b9db925d66c4935ca24796e6ee255c9d
rudalson <rudalson@gmail.com>
2020-02-23 16:29:20 +0900

    added first component
    react의 첫번째 컴포넌트 추가

--- App.js
+++ App.js
@@ -6,17 +6,37 @@
  * @flow
  */
 
-import React from 'react';
+import React, {Component} from 'react';
 import {
   StyleSheet,
   View,
   Text,
+  Image,
 } from 'react-native';
+
+class MyImage extends Component {
+  render () {
+    let img = '';
+    if (this.props.type === 'one') {
+      img = require('./assets/son.jpg');
+    } else if (this.props.type === 'two') {
+      img = require('./assets/samgyeopsal.jpg');
+    }
+
+    return (
+      <View>
+        <Image source = {img} style={{width:100, height:200}}/>
+      </View>
+    )
+  }
+}
 
 const App: () => React$Node = () => {
   return (
     <View style = {styles.container}>
       <Text style = {styles.hello}>Hello World</Text>
+      <MyImage type = 'one'/>
+      <MyImage type = 'two'/>
     </View>
   );
 };
