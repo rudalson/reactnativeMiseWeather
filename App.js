dc5df1f20e8cf7e38ec239f01bef4adf2a615af7
rudalson <rudalson@gmail.com>
2020-02-23 17:18:27 +0900

    added how to state
    state 사용 예 추가

--- App.js
+++ App.js
@@ -7,15 +7,10 @@
  */
 
 import React, {Component} from 'react';
-import {
-  StyleSheet,
-  View,
-  Text,
-  Image,
-} from 'react-native';
+import {StyleSheet, View, Text, Image, Button} from 'react-native';
 
 class MyImage extends Component {
-  render () {
+  render() {
     let img = '';
     if (this.props.type === 'one') {
       img = require('./assets/son.jpg');
@@ -25,31 +20,58 @@ class MyImage extends Component {
 
     return (
       <View>
-        <Image source = {img} style={{width:100, height:200}}/>
+        <Image source={img} style={{width: 100, height: 200}} />
       </View>
-    )
+    );
   }
 }
 
-const App: () => React$Node = () => {
-  return (
-    <View style = {styles.container}>
-      <Text style = {styles.hello}>Hello World</Text>
-      <MyImage type = 'one'/>
-      <MyImage type = 'two'/>
-    </View>
-  );
-};
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      title: '손흥민',
+    };
+  }
+
+  writeTitle = () => {
+    this.setState({
+      title: '손흥민 부상 심각',
+    },function() {
+      alert('그래서 큰일입니다')
+    });
+  };
+
+  writeKeyword = () => {
+    this.setState({
+      title: '손흥민',
+    });
+  };
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.hello}>Hello World</Text>
+        <MyImage type="one" />
+
+        <Text>{this.state.title}</Text>
+        <Button title={'제목 출력'} onPress={this.writeTitle} />
+        <Button title={'키워드 출력'} onPress={this.writeKeyword} />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
-  container : {
+  container: {
     flex: 1,
-    justifyContent : 'center',
-    alignItems: 'center'
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  hello : {
-    color : 'red'
-  }
+  hello: {
+    color: 'red',
+  },
 });
 
 export default App;
